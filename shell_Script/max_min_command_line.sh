d4aa9c864ceba09b5940549cc773b9cd3b552681
Ritushwar <ritushwarneupane111@gmail.com>
2025-07-21 21:44:37 +0545

    Max and Min from command line

---
+++ shell_Script/max_min_command_line.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Finding min and max number from the given dataset passed by command line argument"
+if [ "$#" -eq 0 ]; then
+   echo "Usage: $0 num1 num2 num3 ..."
+   exit 1
+fi
+
+min=$1
+max=$1
+
+for num in "$@"
+do
+   if [ "$num" -gt "$max" ]; then
+       max=$num
+   fi
+   if [ "$num" -lt "$min" ]; then
+       min=$num
+   fi
+done
+
+echo "Max : $max"
+echo "Min : $min"
