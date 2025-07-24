664136a5b9ad84cb52e686456f7f8b5e0ce54391
Ritushwar <ritushwarneupane111@gmail.com>
2025-07-24 19:01:44 +0545

    fork

---
+++ fork_system_calls/fork.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>  
+
+#include <unistd.h>  
+
+#include <sys/types.h>  
+
+int main()  
+
+{  
+
+fork();  
+
+fork();  
+
+fork();  
+
+printf("Hello World \n");  
+
+return 0;  
+
+}
